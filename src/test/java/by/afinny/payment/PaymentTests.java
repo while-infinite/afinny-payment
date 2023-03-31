f290e0364de40852747a44d6a9098329f828a2ad
Karen Farmanyan <farmanyan.k@yandex.ru>
2023-03-31 13:58:29 +0300

    first commit

---
+++ src/test/java/by/afinny/payment/PaymentTests.java
@@ -0,0 +1,13 @@
+package by.afinny.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
